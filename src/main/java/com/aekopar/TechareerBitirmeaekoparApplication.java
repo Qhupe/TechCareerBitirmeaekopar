41114d301741364b4d02783cbddae7b1e8b5b347
aekopar <hupesat@gmail.com>
2022-07-16 17:12:25 +0300

    Initial commit

---
+++ src/main/java/com/aekopar/TechareerBitirmeaekoparApplication.java
@@ -0,0 +1,30 @@
+package com.aekopar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//exclude:dahil etmemek
+/*
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+*/
+
+
+@SpringBootApplication
+public class TechareerBitirmeaekoparApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechareerBitirmeaekoparApplication.class, args);
+
+        //DevTools
+        System.setProperty("spring.devtools.restart.enabled","false");
+
+        // AWT: JOptionPanel set ayarı
+        System.setProperty("java.awt.headless", "false");
+    }
+
+}
---
+++ src/test/java/com/aekopar/TechareerBitirmeaekoparApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aekopar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechareerBitirmeaekoparApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
