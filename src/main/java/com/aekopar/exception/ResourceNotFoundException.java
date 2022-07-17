7f719c59ff0b7c3852f7f3bfd082da82b2a801d7
aekopar <hupesat@gmail.com>
2022-07-17 20:42:02 +0300

    ResourceNotFoundException

--- src/main/java/com/aekopar/exception/ResourceNotFoundException.java
+++ src/main/java/com/aekopar/exception/ResourceNotFoundException.java
@@ -4,6 +4,8 @@ package com.aekopar.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
+//created my exception
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
